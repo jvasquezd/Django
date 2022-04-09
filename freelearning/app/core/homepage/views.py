7bed5d5bd0050942d3df5cda20349f80ec5120be
Jhonatan David Vasquez Enrique <jvasquezenrique@gmail.com>
2022-04-09 03:11:26 -0500

    Eliminando los proyectos

--- freelearning/app/core/homepage/views.py
+++
@@ -1,8 +0,0 @@
-from re import template
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import TemplateView
-
-class HomeView(TemplateView):
-    template_name = 'pages/index.html'
